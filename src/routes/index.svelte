88457ee83af90d413f1c2c1e62b2d7dfb74ca428
Manuel Serret <manuel-serret@t-online.de>
2021-03-24 16:33:22 +0100

    Add total working time

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,6 +5,11 @@
 	import Task from "../scripts/Task";
 	import TimeSpan from "../scripts/TimeSpan";
 	import { tasks } from "../stores/tasks";
+	import { DateTime, Duration } from "luxon";
+
+	let totalDuration = Duration.fromMillis(0);
+
+	$: $tasks, overallDuration();
 
 	onMount(() => {
 		// Enable navigation prompt
@@ -13,27 +18,30 @@
 		};
 	});
 
+	function overallDuration() {
+		totalDuration = Duration.fromMillis(0);
+		$tasks.forEach((task) => {
+			task.timespans.forEach((ts) => {
+				if (ts.from !== "" && ts.to !== "") {
+					var from = DateTime.fromFormat(ts.from, "HH:mm");
+					var to = DateTime.fromFormat(ts.to, "HH:mm");
+					let d = to.diff(from);
+					totalDuration = totalDuration.plus(d);
+				}
+			});
+		});
+	}
+
 	function addTask() {
 		let t = new Task();
 		t.timespans.push(TimeSpan.fromNow());
 		$tasks.push(t);
 		$tasks = $tasks;
 	}
-
-	
 </script>
 
-<style>
-	.main {
-		padding: 20px;
-	}
-
-	.add-task {
-		margin-top: 20px;
-	}
-</style>
-
 <div class="main">
+	<span>Total working time {totalDuration.toFormat("hh:mm")}</span>
 	<div class="tasks">
 		{#each $tasks as task, i}
 			{#if $tasks.length - 1 === i}
@@ -44,6 +52,18 @@
 		{/each}
 	</div>
 	<div class="add-task">
-		<button class="button is-primary" on:click={addTask}>Add new Task</button>
+		<button class="button is-primary" on:click={addTask}
+			>Add new Task</button
+		>
 	</div>
 </div>
+
+<style>
+	.main {
+		padding: 20px;
+	}
+
+	.add-task {
+		margin-top: 20px;
+	}
+</style>
